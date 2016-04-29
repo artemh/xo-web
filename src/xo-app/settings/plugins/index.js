440f647e79026c735c760070d5aab879b14a3344
wescoeur <ronan.abhamon@gmail.com>
2016-04-29 14:53:51 +0200

    Init.

---
+++ src/xo-app/settings/plugins/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import GenericInput from 'json-schema-input'
+
+export default class Plugins extends Component {
+  render () {
+    return <p>Hello</p>
+  }
+}
